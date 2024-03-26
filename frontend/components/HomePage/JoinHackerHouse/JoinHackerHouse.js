7dc4bc387c6ef576d1a155c50cd65374146fc458
Lameed88 <moradeyor88@gmail.com>
2024-03-26 14:07:27 +0100

    joinhackerhouse

--- frontend/components/HomePage/JoinHackerHouse/JoinHackerHouse.js
+++ frontend/components/HomePage/JoinHackerHouse/JoinHackerHouse.js
@@ -1,33 +1,41 @@
-const JoinHackerHouse = () => {
+
+
+const HackerHouseHero = () => {
   return (
-    <>
-      <div className=" font-serif w-[1440px] h-[413px] bg-[#f5f3f5] bg-opacity-100 ">
-        <div className="mx-[50px] flex flex-row justify-between items-start pt-[101px]xcv">
-          <div className="flex flex-col justify-center gap-[48px] w-[570px]  ">
-            <div className=" text-[48px] font-[400px] gap-[10px] ">
-              <span className="text-[#252A24]"> Join our </span>{" "}
-              <span className="text-[#FC7C13]">Hacker House </span>
-              <div className="w-[570px] mb-[38px] text-[16px]">
-                Register for our Hacker House and stand a chance of wining huge
-                prices consectetur adipiscing elit. Nunc vulputate libero et
-                velit interdum, ac aliquet odio mattis.
-              </div>
-            </div>
-            <div className="border border-[#C54809] rounded w-[355px] p-[10px] h-[55px] text-center font-poppins font-bold text-[#C54809] hover:bg-[white] cursor-pointer ">
+    <div className="font-serif bg-[#f5f3f5]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 md:py-24">
+        <div className="flex flex-col-reverse md:flex-col-reverse  lg:flex-row md items-center justify-between">
+
+        <div className="w-full  md:mb-[39px]  ">
+            <h2 className="text-2xl md:text-[36px] lg:text-[48px] text-gray-800 font-400 md:text-center lg:text-start mb-4 flex justify-center lg:justify-start ">
+              Join our  <span className="text-orange-500 ml-2">Hacker House</span>
+            </h2>
+            <p className="text-base md:text-[14px] lg:text-[16px] font-300 md:w-full   lg:h-[57px] md:h-[34px] text-gray-600 lg:mb-[48px] md:mb-[20px] ">
+              Register for our Hacker House and stand a chance of winning huge
+              prizes. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+            </p>
+
+            <div className="flex justify-center lg:justify-start ">
+
+            <button className="w-[353px] lg:mt-[48px] md:justify-center md:mt-[20px] mt-[20px] h-[55px] py-2 px-4 rounded-md  font-400 hover:bg-white text-orange-500 border border-orange-500">
               Read more
+            </button>
             </div>
           </div>
 
-          <div className="">
+          <div className="lg:w-full md:w-[490px] md:mb-[39px]  mr-0 md:mr-12 mb-8">
             <img
               src="hackerhousehero_img/Working 1.png"
-              class="absolute top-[46px] left-[794px]"
+              className="w-full h-auto"
+              alt="Working at Hacker House"
             />
           </div>
+
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default JoinHackerHouse;
+export default HackerHouseHero;
