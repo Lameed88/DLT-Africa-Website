30e7f67bfa5948a8955d2161a4f3b01303ed4ae1
MatexConcept <matexconcept@gmail.com>
2024-04-15 14:47:25 +0100

    update on hackerhouse-eventItenery

--- frontend/components/HackerHouse/HackerHouseEventItinery.js
+++ frontend/components/HackerHouse/HackerHouseEventItinery.js
@@ -1,13 +1,13 @@
 
 const HackerHouseEventItinery = () => {
   return (
-    <div className='h-[745px]  border-b border-solid border-black border-opacity-30 bg-[#031700] flex-shrink-0'>
+    <div className='  border-b border-solid border-black border-opacity-30 bg-[#031700] flex-shrink-0'>
       <div>
-      <h1 className='text-[#F7FCFE] font-Serif text-[48px] pt-[57px] text-center  font-normal font-serif '>Event Itinerary</h1>
+      <h1 className='text-[#F7FCFE] font-Serif text-[48px] pt-[57px] text-center  font-normal font-serif  mb-10'>Event Itinerary</h1>
       </div>
 
-      <div className='flex  justify-center space-x-20 mt-[60px]'>
-        <div className='inline-flex flex-col items-start gap-10'>
+      <div className='flex flex-col  justify-center items-center xl:flex-row gap-20 lg:flex-row md:flex-row p-5 '>
+        <div className='inline-flex flex-col items-start gap-10 '>
         <h4 className='text-[#FC7C13] font-poppins text-[24px] font-medium leading-[120%] opacity-75'>Date: <span className='opacity-75 text-[#F7FCFE] font-poppins text-xl font-medium leading-relaxed'>March - December</span></h4>
 
           <div className='flex flex-col items-start gap-3'>
@@ -34,8 +34,8 @@ const HackerHouseEventItinery = () => {
           </div>
         </div>
       </div>
-      <div className='flex justify-center mt-20'>
-  <button className="w-[353px] h-[55px] p-[10px] flex justify-center items-center gap-[10px] rounded-lg  bg-orange-500 text-[#F7FCFE] sm:w-[250px] sm:h-[45px] sm:text-sm md:w-[300px] md:h-[50px] md:text-base lg:w-[400px] lg:h-[60px] lg:text-lg ">
+      <div className='flex justify-center mt-20 mb-20'>
+  <button className="w-[353px] h-[55px] p-[10px] flex justify-center items-center gap-[10px] rounded-lg  bg-orange-500 text-[#F7FCFE] sm:w-[350px] sm:h-[60px] sm:text-sm md:w-[300px] md:h-[50px] md:text-base lg:w-[400px] lg:h-[60px] lg:text-lg ">
     Sign up for Hacker house
   </button>
 </div>
