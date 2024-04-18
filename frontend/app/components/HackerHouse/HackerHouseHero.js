983eb3b51df62779cf5a88440779d851fca9fd29
Lameed88 <moradeyor88@gmail.com>
2024-04-18 14:31:20 +0100

    first commit

--- frontend/app/components/HackerHouse/HackerHouseHero.js
+++ frontend/app/components/HackerHouse/HackerHouseHero.js
@@ -10,61 +10,7 @@ import Component282 from "../../../public/images/Component282.png";
 const HackerHouseHero = () => {
   return (
     <>
-      {/* <div className=" font-serif Max-w-[1440px] h-[640px] bg-[#031700] flex-wrap justify-end overflow-[hidden]">
-        <div className="">
-          <Image className="absolute top-[260px] left-[482px]  " src={Group1} />
-          <Image src={Group2} className="absolute top-[260px] left-[850px] " />
-        </div>
-        <h2 className="text-[#F7FCFE] text-[48px] font-[400px] flex justify-center pt-[209px] gap-[10px] ">
-          Hacker House
-        </h2>
 
-        <p className="text-[#FC7C13] text-[20px] font-[400px] flex justify-center">
-          sponsored by Lisk
-        </p>
-        <div className="">
-          <p1 className="text-[#F7FCFE] text-[20px] font-[400px] flex justify-center opacity-0.75 Row-gaps-[40px] mb-5">
-            Date: mattis Location: mattismattis
-          </p1>
-        </div>
-        <div className="flex flex-row justify-center gap-20 ">
-          <div className="text-[16px] border border-[#C54809] rounded-[10px] w-[355px] p-[10px] h-[55px] flex items-center justify-center font-poppins font-bold text-[#F7FCFE] bg-[#FC7C13] hover:bg-[#f19d53] cursor-pointer ">
-            Sign up for Hacker House
-          </div>
-          <button className="border border-[#C54809] rounded-[10px] w-[196px] p-[10px] h-[55px] text-center font-poppins font-bold text-[#C54809] hover:bg-[white] cursor-pointer ">
-            view agenda
-          </button>
-        </div>
-        <div className=" ">
-          <Image
-            src={Component23}
-            className="absolute top-[70px] left-[-94] "
-          />
-          <Image
-            src={Component24}
-            className="absolute top-[390px] left-[62px]"
-          />
-          <Image
-            src={Component242}
-            className="absolute top-[597px] left-[247px]"
-          />
-          <Image
-            src={Component27}
-            className="absolute top-[72px] left-[1200px]"
-          />
-          <Image
-            src={Component28}
-            className="absolute top-[429px] right-[62.7px]"
-          />
-          <Image
-            src={Component282}
-            className="absolute top-[597px] right-[245.7px]"
-          />
-        </div>
-      </div>
-    </>
-  );
-}; */}
   <div className="font-serif lg:w-[1440px] md:w-[834px] h-[640px] bg-[#031700] flex-wrap justify-end overflow-auto ">
       <div className="flex justify-between ">
         <div className=" ">
@@ -72,7 +18,7 @@ const HackerHouseHero = () => {
             src="hackerheroleft.svg"
             className="lg:w-[374px] md:w-[187px] w-[93px] mr-4 animate-blink hidden md:block "
           />
-        </div>
+        </div> 
 
         <div className="">
           <img
